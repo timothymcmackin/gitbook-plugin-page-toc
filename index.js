c3f3bd932b9655465ac9dbe019a8f027001e2ee1
Tim McMackin <timothymcmackin@yahoo.com>
2018-02-23 21:53:32 +0000

    proof of concept for reading front matter

--- index.js
+++ index.js
@@ -3,5 +3,13 @@ module.exports = {
     assets: "./assets",
     css: ["page-toc.css"],
     js: ["anchor-3.1.1.min.js","page-toc.js"]
+  },
+  hooks: {
+    "page:before": function(page) {
+        if (page['showToc']) {
+        page.content = "# TOC: " + page['showToc'] + "\n" +page.content;
+        }
+        return page;
+    }
   }
 }
